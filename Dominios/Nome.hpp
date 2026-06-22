28f80dbd2c17ea229051b2e82a867b6ab096a9ea
GeneralChild <38739560+GeneralChild@users.noreply.github.com>
2026-06-21 23:59:22 -0300

    Update Nome.hpp

--- Dominios/Nome.hpp
+++ Dominios/Nome.hpp
@@ -14,7 +14,12 @@ public:
     /**
      * @brief Define o valor do nome.
      * @param valor Nome no formato válido.
-     * @throw std::invalid_argument se o formato for inválido.
+     * @brief Texto com até 10 caracteres.
+     * @brief Caracter pode ser letra maiúscula(A-Z), letra minúscula(a-z) ou espaço em branco; espaço em branco é seguido por letra; o primeiro e o último caracter não podem ser espaços em branco.
+     * @throw std::invalid_argument Exceção é lançada caso tamanho do nome não esteja no intervalo entre 1 e 10 caracteres.
+     * @throw std::invalid_argument Exceção é lançada caso nome inicie ou termine com um espaço.
+     * @throw std::invalid_argument Exceção é lançada caso um espaço não seja seguido por uma letra.
+     * @throw std::invalid_argument Exceção é lançada caso algum caracter não for uma letra ou um espaço.
      */
     void setValor(const std::string& valor);
 
