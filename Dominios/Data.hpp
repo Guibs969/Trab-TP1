e0152d92f95db936d10931a35a84f5f4efce77bc
GeneralChild <38739560+GeneralChild@users.noreply.github.com>
2026-06-21 23:55:00 -0300

    Update Data.hpp

--- Dominios/Data.hpp
+++ Dominios/Data.hpp
@@ -12,7 +12,16 @@ private:
 public:
     /**
      * @brief Define o valor da data.
-     * @param valor Data em formato string.
+     * @param valor Data em formato string, na ordem DIA/MÊS/ANO.
+     * @param DIA é número de 1 a 31.
+     * @param MÊS é número 01,02,03,04,05,06,07,08,09,10,11 ou 12.
+     * @param ANO é número de 2000 a 2999.
+     * @param anos bissextos são considerados.
+     * @throw std::invalid_argument Exceção é lançada se formato DD/MM/AAAA não for seguido.
+     * @throw std::invalid_argument Exceção é lançada se dia, mês ou ano não forem inseridos.
+     * @throw std::invalid_argument Exceção é lançada caso ano não esteja entre 2000 e 2999.
+     * @throw std::invalid_argument Exceção é lançada caso mês não esteja entre 01 e 12.
+     * @throw std::invalid_argument Exceção é lançada caso dia inserido esteja fora do intervalo para o mês inserido.
      */
     void setValor(const std::string& valor);
 
