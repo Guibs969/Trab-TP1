2961b364b5735ad2504b6f3e44c57d17970f8e5d
GeneralChild <38739560+GeneralChild@users.noreply.github.com>
2026-06-22 00:01:39 -0300

    Update Senha.hpp

--- Dominios/Senha.hpp
+++ Dominios/Senha.hpp
@@ -14,7 +14,12 @@ public:
     /**
      * @brief Define o valor da senha.
      * @param valor Senha no formato válido.
-     * @throw std::invalid_argument se o formato for inválido.
+     * @brief Caracter pode ser letra(a-z ou A-Z) ou dígito(0-9); letra não pode ser seguida por letra, dígito não pode ser seguido por dígito.
+     * @brief Deve existir pelo menos uma letra minúscula(a-z), uma letra maiúscula(A-Z) e um dígito(0-9).
+     * @throw std::invalid_argument Exceção é lançada caso tamanho da senha seja diferente de 6 caracteres.
+     * @throw std::invalid_argument Exceção é lançada caso sejam inseridos caracteres que não sejam letras ou dígitos.
+     * @throw std::invalid_argument Exceção é lançada caso hajam letras seguidas por letras ou dígitos seguidos por dígitos.
+     * @throw std::invalid_argument Exceção é lançada caso senha não contenha pelo menos uma letra maiúscula, uma minúscula e um dígito.
      */
     void setValor(const std::string& valor);
 
